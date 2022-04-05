09acfafcdb0f1a017ac58dd02edb0934db64e12a
kimisak <isak7263@gmail.com>
2022-04-05 23:55:09 +0900

    board 정리

--- src/views/Board.vue
+++ src/views/Board.vue
@@ -98,30 +98,10 @@
 </template>
 
 <script>
-import BasicElements from "./components/BasicElementsSection";
-import Navigation from "./components/NavigationSection";
-import SmallNavigation from "./components/SmallNavigationSection";
-import Tabs from "./components/TabsSection";
-import NavPills from "./components/NavPillsSection";
-import Notifications from "./components/NotificationsSection";
-import TypographyImages from "./components/TypographyImagesSection";
-import JavascriptComponents from "./components/JavascriptComponentsSection";
-import { LoginCard } from "@/components";
-import BoardList from "./components/BoardListSection.vue";
 import { Pagination } from "@/components";
 
 export default {
   components: {
-    // BasicElements,
-    // Navigation,
-    // SmallNavigation,
-    // Tabs,
-    // NavPills,
-    // Notifications,
-    // TypographyImages,
-    // JavascriptComponents,
-    // LoginCard
-    // BoardList
     Pagination
   },
   name: "board",
@@ -191,10 +171,11 @@ export default {
       })
     },
     prevPagination() {
-      alert("prev");
+      if (this.defaultPagination <= 1) return;
+      this.defaultPagination -= 1;
     },
     nextPagination() {
-      alert("next");
+      this.defaultPagination += 1;
     }
 
   },
